587cb7ccdfd558f42d914fc3a661c09f59489477
Sathish-M12 <sathishmechon.111@gmail.com>
2024-09-26 23:39:41 +0530

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
-const App = () => {
+
+const Hello = () => {
   return <h1>Hello World!</h1>
+
+}
+
+
+
+
+
+const App = () => {
+  return (
+    <div>
+      <Hello />
+    </div>
+  )
 }
 
 export default App;
